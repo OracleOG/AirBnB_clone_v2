736f39239dfad60373d939fc870732678ea544d7
OracleOG <emmanuelnwafor212@gmail.com>
2025-01-17 19:36:19 +0100

    task 6.6

--- models/city.py
+++ models/city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from models.base_model import Base
 from models.place import Place
@@ -13,4 +12,3 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False, )
-    state = relationship('State', cascade='all, delete, delete-orphan',backref='cities')
